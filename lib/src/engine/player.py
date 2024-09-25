7d7b48278db5afa22ad3966b829b21213d5cb430
JoshSims77 <125834861+JoshSims77@users.noreply.github.com>
2024-09-24 22:19:24 -0500

    Update player.py

--- lib/src/engine/player.py
+++ lib/src/engine/player.py
@@ -7,5 +7,10 @@ class Player:
         self.hand = []    #initialize an empty list for the players hand
 
 
-    def __str__(self):
+    def __str__(self): #for testing purposes of the players hand
         return f"{self.name}'s hand: {', '.join([str(card) for card in self.hand])}"
+    
+
+    def add_player(self):
+        PlayerList = []
+        Playerlist = self.append #idea for tomo, too tired to build on this today
